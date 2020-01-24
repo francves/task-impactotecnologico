2a57751e22a69602e51fd5dcea5b45768c33f260
Francisco Velasquez <francves1711@gmail.com>
2020-01-24 00:34:57 -0400

    Inicializar formulario, validar, implementar edicion y creacion de usuarios

--- src/app/core/components/users/users-form/users-form.component.ts
+++ src/app/core/components/users/users-form/users-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { User } from 'src/app/core/models/user';
+import { UsersService } from './../../../services/users.service';
 
 @Component({
   selector: 'app-users-form',
@@ -16,14 +18,16 @@ export class UsersFormComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<UsersFormComponent>,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private usersService: UsersService
   ) {
     this.createForm(fb);
   }
 
   ngOnInit() {
     console.log("Usuarios: ", this.data)
-    this.action = this.data.action
+    this.action = this.data.action;
+    this.initForm();
   }
 
   /**
@@ -45,13 +49,71 @@ export class UsersFormComponent implements OnInit {
     });
   }
 
+  initForm() {
+    let user = this.data.userData
+    if (user){
+      this.form.patchValue({
+        first_name: user.first_name,
+        last_name: user.last_name,
+        gender: user.gender,
+        dob: user.dob,
+        email: user.email,
+        phone: user.phone,
+        website: user.website,
+        address: user.address
+      });
+    }
+  }
+
   onSubmit() {
     console.log("Enviando...")
     if(this.form.valid){
       console.log("Formulario valido!")
+      let _user = {
+        id: this.data.userData ? this.data.userData.id : null,
+        first_name: this.form.get('first_name').value,
+        last_name: this.form.get('last_name').value,
+        gender: this.form.get('gender').value,
+        dob: this.form.get('dob').value,
+        email: this.form.get('email').value,
+        phone: this.form.get('phone').value,
+        website: this.form.get('website').value,
+        address: this.form.get('address').value
+      }
+      if(this.action === "Editar"){
+        this.editUser(_user)
+      } else {
+        this.addUser(_user)
+      }
     } else {
       console.log("Formulario invalido!")
     }
+  }
+
+  editUser(userData: User){
+    this.usersService.editUser(userData)
+      .subscribe(editUser => {
+        console.log("Usuario actualizado", editUser)
+      }, error => {
+        this.errorMessage = <any>error
+      },
+      () => {
+        this.dialogRef.close(true)
+      }
+      )
+  }
+
+  addUser(userData: User){
+    this.usersService.addUser(userData)
+      .subscribe(newUser => {
+        console.log("Nuevo usuario", newUser)
+      }, error => {
+        this.errorMessage = <any>error
+      },
+      () => {
+        this.dialogRef.close(true)
+      }
+      )
   }
 
 }
--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from './../../../environments/environment';
 
@@ -19,6 +19,21 @@ export class UsersService {
         .set('access-token', this.token)
     const url = `${this.apiUrl}users`;
     return this.http.get<any>(url, {params});
-}
+  }
+
+  addUser(userData): Observable<any> {
+    let headers = new HttpHeaders()
+    .set('Authorization', `Bearer ${this.token}`)
+    const url = `${this.apiUrl}users`;
+    return this.http.post<any>(url, userData, {headers});
+  }
+
+  editUser(userData): Observable<any> {
+    let headers = new HttpHeaders()
+    .set('Authorization', `Bearer ${this.token}`)
+    const url = `${this.apiUrl}users/${userData.id}`;
+    return this.http.put<any[]>(url, userData, {headers});
+  }
+
 }
 
